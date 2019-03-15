a188d28dbcd4ca843ff5c0eb7a51fcda2170ec4f
VishwaYadav <yadavvishwajeet880@gmail.com>
2019-03-15 19:43:35 +0530

    Variable added by vishwa

--- app/src/main/java/abdealibhagat/example/com/workingwithpdf/Newfile.java
+++ app/src/main/java/abdealibhagat/example/com/workingwithpdf/Newfile.java
@@ -4,9 +4,11 @@ public class Newfile {
 
     private int n;
     private int m;
+    private int vishwa;
 
-    Newfile(int n, int m) {
+    Newfile(int n, int m,int vishwa) {
         this.n = n;
         this.m = m;
+	this.vishwa=vishwa;
     }
 }
